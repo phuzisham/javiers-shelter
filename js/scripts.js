5d50e411e5ab457456b942f35308a2aadeaf8b6f
David Hale and Javier Jimenez <javier@>
2017-08-08 09:50:06 -0700

    add buttons, black theme

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+  $("#dark").click(function() {
+    $('body').addClass('dark');
+  });
+  $('#normal').click(function() {
+    $('body').removeClass();
+    //$('body').addClass('blue');
+  });
+});
